2db838d25bf0bb164ba7011a871409b49be25044
CRISTHIAN REYNA <creyna@tecopesca.com>
2024-01-14 11:34:08 -0500

    Agregando archivos iniciales

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Seleccionar el elemento de video
+  const video = document.querySelector("video");
+
+  // Reproducir el video cuando se carga la página
+  video.play();
+
+  // Puedes agregar más funcionalidades aquí si es necesario
+});
